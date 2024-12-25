5956de8e710cd184ed73d56d88567c64d933c57a
Janek <development@superyu.xyz>
2024-12-25 21:04:14 +0100

    wip: add icon to windows build

---
+++ osu7-client/build.rs
@@ -0,0 +1,10 @@
+#[cfg(target_os="windows")]
+fn main() -> std::io::Result<()> {
+    if std::env::var_os("CARGO_CFG_WINDOWS").is_some() {
+        winresource::WindowsResource::new()
+            // This path can be absolute, or relative to your crate root.
+            .set_icon("../assets/icon.ico")
+            .compile()?;
+    }
+    Ok(())
+}
